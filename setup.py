07b2a19c5ac25fb884653bf5cf3c20ed51e30487
TejaRekadi <tejarekadi7@gmail.com>
2025-12-10 16:44:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = "LangQ - Your AI-Powered Interview Prep Curator",
     version= '0.0.0',
-    author="Dileep Kanumuri, Guru Saranya Gopireddy, Satwik Dikkala",
-    author_email="dileepkanumuri.adm@gmail.com, saranyagopireddy2801@gmail.com, satwikds@gmail.com",
+    author="Teja Rekadi, Shiva Chigurupati, Chakri CHigurupati",
+    author_email="tejarekadi7@gmail.com, @gmail.com, @gmail.com",
     packages= find_packages(),
     install_requires = []
 )
